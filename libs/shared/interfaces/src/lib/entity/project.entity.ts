5e8f19a383ccd203c829695b53654b2cf4c9dcfe
Nathael Bonnal <pro.nathaelbonnal@gmail.com>
2023-12-03 13:13:25 +0100

    feat(interfaces): update

---
+++ libs/shared/interfaces/src/lib/entity/project.entity.ts
@@ -0,0 +1,8 @@
+export interface ProjectEntity {
+  id: string
+  title: string
+  description: string
+  devise: string
+  created_at: string
+  updated_at: string
+}
---
+++ libs/shared/interfaces/src/lib/states/projects.interface.ts
@@ -0,0 +1,4 @@
+import { ProjectEntity } from "../entity";
+import { DefaultEntityState } from "./default-entity-state.interface";
+
+export interface ProjectsState extends DefaultEntityState<ProjectEntity> {}
