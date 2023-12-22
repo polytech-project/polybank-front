71fa3eca0575ade00f27f42a46bde88b8a36554d
Nathael Bonnal <pro.nathaelbonnal@gmail.com>
2023-12-22 10:58:31 +0100

    feat(queries): add `useCreateTransaction`

--- libs/domains/projects/src/lib/projects.queries.ts
+++ libs/domains/projects/src/lib/projects.queries.ts
@@ -1,10 +1,8 @@
-import {useQuery, useQueryClient} from "react-query";
+import {useMutation, useQuery, useQueryClient} from "react-query";
 import {apiClient} from "@polybank/api-client";
 import {ProjectEntity, ProjectResponse} from "@polybank/interfaces";
 
 export const useFetchProjects = () => {
-  const queryClient = useQueryClient()
-
   return useQuery(
     ['projects'],
     async (): Promise<ProjectEntity[]> => {
@@ -23,10 +21,27 @@ export const useFetchProject = (
   projectId: string
 ) => {
   return useQuery(
-    ['project', projectId],
+    ['projects', projectId],
     async () => {
-      const response = await apiClient.get<ProjectEntity>(`/projects/${projectId}`).build()
+      const response = await apiClient.get<{ project: ProjectEntity, expenses: number, balances: any[]}>(`/projects/${projectId}`).build()
       return response.data
     }
   )
+}
+
+export const useCreateTransaction = () => {
+  const queryClient = useQueryClient()
+  return useMutation(async ({ projectId, data }: { projectId: string, data: {}}) => {
+    const response = await apiClient.post(`/projects/${projectId}/transactions`)
+      .payload(data).build()
+
+    return {
+      ...response.data,
+      projectId
+    }
+  }, {
+    onSuccess: async (data) => {
+      await queryClient.invalidateQueries(['projects', data.projectId])
+    }
+  })
 }
