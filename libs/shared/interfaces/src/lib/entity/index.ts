62dbb62877608cca1bd6c024bf09ca836f562e55
Nathael Bonnal <pro.nathaelbonnal@gmail.com>
2023-12-22 10:58:58 +0100

    feat(interfaces): update interfaces

--- libs/shared/interfaces/src/lib/entity/index.ts
+++ libs/shared/interfaces/src/lib/entity/index.ts
@@ -1,3 +1,3 @@
 export * from './user.entity'
 export * from './project.entity'
-export * from './expense.entity'
+export * from './transaction.entity'
--- libs/shared/interfaces/src/lib/entity/transaction.entity.ts
+++ libs/shared/interfaces/src/lib/entity/transaction.entity.ts
@@ -1,12 +1,18 @@
-import { UserEntity } from './user.entity'
+import {UserEntity} from "./user.entity";
+import {ProjectEntity} from "./project.entity";
 
 export interface TransactionEntity {
   id: string
   title: string
   amount: number
   project_id: string
+  type: string
   paid_by: string
   user_id: string
-  user: UserEntity
-  paidByUser: UserEntity
+  user?: UserEntity
+  users?: UserEntity[]
+  paidByUser?: UserEntity
+  project?: ProjectEntity
+  created_at: string
+  updated_at: string
 }
