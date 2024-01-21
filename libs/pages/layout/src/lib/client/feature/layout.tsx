f34702a97ee99933937b6736ecc72e943b19b897
Nathael Bonnal <pro.nathaelbonnal@gmail.com>
2024-01-21 21:54:42 +0100

    update

--- libs/pages/layout/src/lib/client/feature/layout.tsx
+++ libs/pages/layout/src/lib/client/feature/layout.tsx
@@ -6,24 +6,16 @@ import { AppDispatch } from '@polybank/state/store'
 // eslint-disable-next-line @nx/enforce-module-boundaries
 import { fetchUser } from '@polybank/domains/users'
 import {fetchProjects} from "@polybank/domains/projects";
-import {useFetchNews} from "@polybank/domains/news";
+
 
 export function Layout({ children }: PropsWithChildren) {
   const dispatch = useDispatch<AppDispatch>()
-
-  const { data } = useFetchNews({
-    actif: true
-  })
 
   useEffect(() => {
     dispatch(fetchUser())
 
     dispatch(fetchProjects())
   }, [dispatch])
-
-  useEffect(() => {
-    console.log(data)
-  }, [data])
 
   return (
     <LayoutPage>
